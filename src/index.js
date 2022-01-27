c742289f84ad64d0a4231df8de2d82e29b68070e
YuriyKievskiy <kievskiy.y.a@gmail.com>
2022-01-27 19:00:31 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,47 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+let wordsArr = [];
 
+function decode(expr) {
+    let wordsArr=[];
+    let morseCode = [];
+    let result = [];
+
+    let word = expr.match(/.{1,10}/g); //Разбиваем на массив по 10 символов
+
+    word.map(function(key, index) {         //Разбиваем на подмассив по 5 пар символов
+        let keys = key.match(/.{1,2}/g);
+        wordsArr.push(keys);
+    });
+            //переводим '00' '10' '11' в код морзе
+    wordsArr.forEach(function(key) {
+        let morse = [];
+        for(let i = 0; i < key.length; i++) {
+            if(key[i] === '00'){
+                morse.push('')
+            }else if(key[i] === '10'){
+                morse.push('.')
+            }else if(key[i] === '11'){
+                morse.push('-')
+            }else if(key[i] === '**'){
+                morse.push('*')
+            }
+        }
+        morseCode.push(morse.join(''));
+    })
+
+    for( let i = 0; i < morseCode.length; i++ ) {
+        let bord = " ";
+        console.log(morseCode[i])
+        if( morseCode[i] === "*****") {
+            result.push(bord)
+        }else if(morseCode[i] in MORSE_TABLE) {
+            result.push(MORSE_TABLE[morseCode[i]])
+        }
+    }
+    return result.join('');
+}
 module.exports = {
     decode
 }
